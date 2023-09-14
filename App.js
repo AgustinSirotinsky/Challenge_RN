772df8bf7693596923b01384b05fcf6a8f408f34
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-09-14 12:02:13 -0300

    loolsassas

--- App.js
+++ App.js
@@ -1,29 +1,54 @@
+//Expo
 import { StatusBar } from 'expo-status-bar';
+
+//React
 import { ActivityIndicator, FlatList, StyleSheet, Text, View } from 'react-native';
 import React, {useEffect, useState} from 'react';
 
+//Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
 export default function App() {
+  //Delay para que la api llegue a tiempo !!
+  const delay = ms => new Promise(
+    resolve => setTimeout(resolve, ms)
+  );
+
   const [platos, setPlatos] = useState([])
   const getComida = async () => {
     try {
-      const response = await fetch('https://api.spoonacular.com/food/menuItems/1?apiKey=389b8091e46e4627aba19a5007f6ad34');
+      const response = await fetch('https://api.spoonacular.com/food/menuItems/123434?apiKey=8e1d9d5270a7426dba8626b384a2d28c');
       const json =await response.json();
       setPlatos(json)
+      // console.log(platos)
     }
     catch (error) {
       console.error(error);
+    }
+    finally {
     }
   }
   
   useEffect(() => {
     getComida();
+    makeRequest();
   }, []);
 
   return (
     <View style={styles.container}>
-      {console.log(platos)}
-      <Text>{platos.title}</Text>
-      <StatusBar style="auto" />
+    {console.log(platos.generatedText)}
+      <Card style={{ width: '15rem' }}>
+      <Card.Img variant="top" src={platos.image} />
+      <Card.Body>
+        <Card.Title>{platos.title}</Card.Title>
+        <Card.Text>
+          {platos.generatedText}
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
     </View>
   );
 }
