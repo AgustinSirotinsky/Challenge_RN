b5045afc88c5e7f10a85c42198e64b49846fdfcf
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-09-20 10:33:04 -0300

    routes

--- App.js
+++ App.js
@@ -1,60 +1,14 @@
-//Expo
-import { StatusBar } from 'expo-status-bar';
-
 //React
-import { ActivityIndicator, FlatList, StyleSheet, Text, View } from 'react-native';
-import React, {useEffect, useState} from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-//Bootstrap
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+//Routes
+import Home from './src/routes/Home';
 
 export default function App() {
-  //Delay para que la api llegue a tiempo !!
-  const delay = ms => new Promise(
-    resolve => setTimeout(resolve, ms)
-  );
-
-  const [platos, setPlatos] = useState([])
-  const getComida = async () => {
-    try {
-      const response = await fetch('https://api.spoonacular.com/food/menuItems/123434?apiKey=8e1d9d5270a7426dba8626b384a2d28c');
-      const json =await response.json();
-      setPlatos(json)
-      // console.log(platos)
-    }
-    catch (error) {
-      console.error(error);
-    }
-    finally {
-    }
-  }
-  
-  useEffect(() => {
-    getComida();
-    makeRequest();
-  }, []);
-
   return (
-    <View style={styles.container}>
-    {console.log(platos.generatedText)}
-      <Card style={{ width: '15rem' }}>
-      <Card.Img variant="top" src={platos.image} />
-      <Card.Body>
-        <Card.Title>{platos.title}</Card.Title>
-        <Card.Text>
-          {platos.generatedText}
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    
-  },
-});
+    <BrowserRouter>
+      <Routes>
+          <Route index element={<Home />}></Route>
+      </Routes>
+    </BrowserRouter>
+  )}
---
+++ src/routes/Home.jsx
@@ -0,0 +1,60 @@
+//Expo
+import { StatusBar } from 'expo-status-bar';
+
+//React
+import { ActivityIndicator, FlatList, StyleSheet, Text, View } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import { Link } from 'react-router-dom';
+
+//Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+export default function Home() {
+  //Delay para que la api llegue a tiempo !!
+const delay = ms => new Promise(
+    resolve => setTimeout(resolve, ms)
+);
+
+const [platos, setPlatos] = useState([])
+const getComida = async () => {
+    try {
+    const response = await fetch('https://api.spoonacular.com/food/menuItems/32486?apiKey=389b8091e46e4627aba19a5007f6ad34');
+    const json =await response.json();
+        setPlatos(json)
+      // console.log(platos)
+    }
+    catch (error) {
+    console.error(error);
+    }
+    finally {
+    }
+}
+
+useEffect(() => {
+    getComida();
+}, []);
+
+return (
+    <View style={styles.container}>
+    {console.log(platos)}
+    <Card style={{ width: '15rem' }}>
+    <Card.Img variant="top" src={platos.image} />
+    <Card.Body>
+        <Card.Title>{platos.title}</Card.Title>
+        <Card.Text>
+        {platos.generatedText}
+        </Card.Text>
+        <Button variant="primary">Read More</Button>
+    </Card.Body>
+    </Card>
+    </View>
+);
+}
+
+const styles = StyleSheet.create({
+container: {
+    
+},
+});
