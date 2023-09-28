5facb73f4f8a139610309b6351f156634e5a8388
Agustin Sirotinsky <102998952+AgustinSirotinsky@users.noreply.github.com>
2023-09-28 12:00:02 -0300

    Update Information.jsx

--- src/routes/Information.jsx
+++ src/routes/Information.jsx
@@ -2,7 +2,7 @@
 import { StatusBar } from 'expo-status-bar';
 
 //React
-import {StyleSheet, Text, View, Image } from 'react-native';
+import {StyleSheet, Text, View } from 'react-native';
 import React, {useEffect, useState} from 'react';
 import { Link, useParams } from 'react-router-dom';
 
@@ -10,6 +10,8 @@ import { Link, useParams } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import Image from 'react-bootstrap/Image';
+
 export default function Information() {
     //consigue el platoid
     const params=useParams();
@@ -17,7 +19,7 @@ export default function Information() {
     const [plato, setPlato] = useState({})
     const getComida = async () => {
         try {
-        const response = await fetch(`https://api.spoonacular.com/food/menuItems/${params.platoid}?apiKey=389b8091e46e4627aba19a5007f6ad34`);
+        const response = await fetch(`https://api.spoonacular.com/food/menuItems/${params.platoid}?apiKey=ca06425c7eef4240b7cc759f05e335cb`);
         const json =await response.json();
             console.log(json.images[0])
             setPlato(json)
@@ -34,21 +36,28 @@ export default function Information() {
 
     return (
         <View style={styles.container}>
+        <Link to={`/`} style={{ color: '#000000' }}><Button style={styles.botonHome}>Home</Button></Link>
         <Text style={styles.title}>{plato.title}</Text>
-        <Image source={{uri: plato.images[0]}} />
         </View>
     )
 }
 const styles = StyleSheet.create({
     container: {
-        
     },
     title: {
+        fontWeight: 'bold',
         textAlign: 'center',
-        fontSize:16,
+        marginTop:50,
+        fontSize:20,
     },
     titleImage: {
 
     },
+    botonHome: {
+        position: 'absolute',
+        left: 0,
+        up: 0
+    },
     });
+    
     
